e535ecb6694f3ee26da7072cb1cbe71eb9889023
joongau <gaultier.jonathan@gmail.com>
2019-02-08 02:29:23 +0100

    révisions

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,27 +6,27 @@ Comment.destroy_all
 City.destroy_all
 Tag.destroy_all
 
-10.times do |city|
-	City.create!(name: Faker::TvShows::GameOfThrones.city, zip_code: Faker::Address.zip_code)
+20.times do |city|
+	City.create!(name: Faker::Movies::LordOfTheRings.location, zip_code: Faker::Address.zip_code)
 end
 
-10.times do |user|
-	User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::TvShows::RickAndMorty.quote, email: Faker::Internet.email, age: rand(10..74), city_id: City.all.sample.id, password: "claudia")
+20.times do |user|
+	User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::TvShows::Community.quotes, email: Faker::Internet.email, age: rand(10..74), city_id: City.all.sample.id, password: "azerty")
 end
 
-24.times do |gossip|
-	Gossip.create!(title: Faker::Lorem.characters(10), content: Faker::ChuckNorris.fact, user_id: User.all.sample.id)
+50.times do |gossip|
+	Gossip.create!(title: Faker::Book.title, content: Faker::GreekPhilosophers.quote, user_id: User.all.sample.id)
 end
 
-10.times do |tag|
+20.times do |tag|
 	Tag.create!(title: Faker::Book.genre)
 end
 
-20.times do |gossiptag|
+30.times do |gossiptag|
 	GossipsTag.create!(gossip_id: Gossip.all.sample.id, tag_id: Tag.all.sample.id)
 end
 
 
-20.times do |comment|
+30.times do |comment|
 	Comment.create!(content: Faker::TvShows::FamilyGuy.quote, user_id: User.all.sample.id, gossip_id: Gossip.all.sample.id)
 end
